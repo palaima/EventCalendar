f86844953afd111c4c9859b99e70355158e3f10e
Mantas Palaima <palaima.mantas@gmail.com>
2016-03-26 17:43:24 +0200

    Bottom sheet for common actions

--- app/src/main/java/io/palaima/calendar/CalendarActivity.java
+++ app/src/main/java/io/palaima/calendar/CalendarActivity.java
@@ -5,8 +5,13 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.text.InputType;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.afollestad.materialdialogs.MaterialDialog;
+import com.kennyc.bottomsheet.BottomSheet;
+import com.kennyc.bottomsheet.BottomSheetListener;
 import com.squareup.sqlbrite.BriteDatabase;
 import com.squareup.sqlbrite.QueryObservable;
 import com.squareup.sqlbrite.SqlBrite;
@@ -35,6 +40,8 @@ public class CalendarActivity extends AppCompatActivity {
 
     private BriteDatabase database;
     private Subscription categoriesUpdateSubscription = Subscriptions.empty();
+
+    private List<MyCategory> categories = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -73,9 +80,74 @@ public class CalendarActivity extends AppCompatActivity {
         super.onDestroy();
     }
 
+    @Override public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_calendar, menu);
+        return true;
+    }
+
+    @Override public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_calendar_add) {
+            calendarOptionsMenu();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
     @OnClick(R.id.fab)
     public void onFabClick() {
         Timber.d("onFabClick");
+
+    }
+
+    public void calendarOptionsMenu() {
+        new BottomSheet.Builder(this)
+            .setSheet(R.menu.menu_calendar_options)
+            .setTitle("Actions")
+            .setListener(new BottomSheetListener() {
+                @Override public void onSheetShown() {
+
+                }
+
+                @Override public void onSheetItemSelected(MenuItem menuItem) {
+                    int itemId = menuItem.getItemId();
+
+                    if (itemId == R.id.action_calendar_add_category) {
+                        openAddCategoryDialog();
+                    } else if (itemId == R.id.action_calendar_add_event) {
+
+                    } else if (itemId == R.id.action_calendar_remove_category) {
+                        openRemoveCategoryDialog();
+                    } else if (itemId == R.id.action_calendar_remove_event) {
+
+                    } else {
+                        throw new IllegalStateException("Menu action is not supported");
+                    }
+                }
+
+                @Override public void onSheetDismissed(int i) {
+
+                }
+            })
+            .show();
+    }
+
+    private void reloadCategories(List<MyCategory> categories) {
+        this.categories.clear();
+        this.categories.addAll(categories);
+        this.calendarView.config()
+            .categories(categories)
+            .set();
+    }
+
+    private void openAddCategoryDialog() {
         new MaterialDialog.Builder(this)
             .cancelable(true)
             .autoDismiss(true)
@@ -95,10 +167,27 @@ public class CalendarActivity extends AppCompatActivity {
             .show();
     }
 
-    private void reloadCategories(List<MyCategory> categories) {
-        calendarView.config()
-            .categories(categories)
-            .set();
-    }
+    private void openRemoveCategoryDialog() {
+        List<String> titles = new ArrayList<>();
 
+        for (MyCategory category : categories) {
+            titles.add(category.getName());
+        }
+
+        new MaterialDialog.Builder(this)
+            .cancelable(true)
+            .autoDismiss(true)
+            .title("Select category to delete")
+            .items(titles)
+            .itemsCallbackSingleChoice(-1, new MaterialDialog.ListCallbackSingleChoice() {
+                @Override public boolean onSelection(MaterialDialog dialog, View itemView, int which, CharSequence text) {
+                    database.delete(CategoryEntity.TABLE_NAME, CategoryEntity._ID + " = " + categories.get(which).getId());
+                    return true;
+                }
+            })
+            .positiveText("Delete")
+            .negativeText("Cancel")
+            .build()
+            .show();
+    }
 }
